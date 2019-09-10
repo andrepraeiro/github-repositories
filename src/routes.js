b7eab0d74559193ecd826b162166ce643603b8cf
Andre Praeiro <andrepraeiro@gmail.com>
2019-09-10 00:27:29 -0400

    initila commit

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import repository from './pages/Repository';
+import main from './pages/Main';
+// import { Container } from './styles';
+
+export default function Reoutes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={main} />
+        <Route path="/repository" component={repository} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
